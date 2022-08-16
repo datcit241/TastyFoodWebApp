ea6dc0094a8755bf13c98815f3528548d045197c
Dat Vo <dat.cit241@gmail.com>
2022-08-16 15:11:51 +0700

    Initial Commit

---
+++ src/main/java/com/data/data_storage/DataStorage.java
@@ -0,0 +1,9 @@
+package com.data.data_storage;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
+public class DataStorage {
+}
